04b7897290492a25f443955dfbe0fc6701725dec
root <root@Liangcheng.localdomain>
2024-12-31 22:54:13 +1100

    sharedlib make 2 still not working, move on

--- week07/lab/sharedlib/make2/include/mymath.h
+++ week07/lab/sharedlib/make2/include/mymath.h
@@ -1,4 +1,3 @@
-// mymyth.h
 #ifndef __MY_MATH_H__
 #define __MY_MATH_H__
 float arraySum(const float *array, size_t size);
--- week07/lab/sharedlib/make2/src/mymath.cpp
+++ week07/lab/sharedlib/make2/src/mymath.cpp
@@ -1,16 +1,15 @@
-// mymath.cpp
 #include <iostream>
 #include "mymath.h"
 
 float arraySum(const float *array, size_t size)
 {
-	if(array == NULL)
-	{
-		std::cerr << "NULL pointer!" << std::endl;
-		return 0.0f;
-	}
-	float sum = 0.0f;
-	for(size_t i = 0; i < size; i++)
-		sum += array[i];
-	return sum;
+    if(array == NULL)
+    {
+        std::cerr << "NULL pointer!" << std::endl;
+        return 0.0f;
+    }
+    float sum = 0.0f;
+    for(size_t i = 0; i < size; i++)
+        sum += array[i];
+    return sum;
 }
